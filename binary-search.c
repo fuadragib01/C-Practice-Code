ac4f6a55e7289c1cb6ceb0a52301065167450779
Fuad Ragib <ragibfuad@gmail.com>
2022-07-21 20:25:20 +0600

    Create binary-search.c

---
+++ binary-search.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int binary_search( int a[], int n, int x ) {
+      int left, right, mid;
+      left = 0;
+      right = n - 1;
+
+      while ( left <= right ) {
+            mid = ( left + right ) / 2;
+            if ( a[mid] == x ) {
+                  return mid;
+            }
+            if ( a[mid] < x ) {
+                  left = mid + 1;
+            } else {
+                  right = mid -1;
+            }
+      }
+      return -1;
+}
+int main() {
+      int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+      int x = 10;
+      int n = 9;
+      if ( -1 == binary_search( nums, n, x ) ) {
+            printf( "Number not found\n" );
+      } else {
+            printf( "Number found\n" );
+      }
+      return 0;
+}
